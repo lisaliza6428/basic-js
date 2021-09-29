51ec64f6c3c0d1ec445ada3bc070f9a6bfa64615
Elizaveta <elizavetaivanushenko@gmail.com>
2021-09-29 10:42:18 +0300

    feat: add solution for carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,7 +18,16 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 export default function dateSample(sampleActivity) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  if (typeof sampleActivity !== 'string' || Number.isNaN(Number(sampleActivity)) || (Number(sampleActivity) < 0)){return false};
+
+ let a = Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD))
+
+ if (a == NaN || a === Infinity || a < 0){
+  return false
+ }
+ else{
+   return a
+ }
+
   
 }
