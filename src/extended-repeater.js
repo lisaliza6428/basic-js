9c9332a1c2cd9c1e222ea5b71a335faa24f0c858
Elizaveta <elizavetaivanushenko@gmail.com>
2021-10-01 11:59:59 +0300

    feat: add solution of emailaddress

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
+export default function repeater(str, options) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+    // remove line with error and write your code here
 }
