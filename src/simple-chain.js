d95e445d70a9b5c97602d5b5ecf6108db9b67a4d
Elizaveta <elizavetaivanushenko@gmail.com>
2021-10-03 22:20:17 +0300

    feat: add solution mac-address

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,8 +6,7 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 export default {
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.length
   },
   addLink(/* value */) {
     throw new NotImplementedError('Not implemented');
--- src/st-matrix-elements-sum.js
+++ src/st-matrix-elements-sum.js
@@ -16,7 +16,7 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be 9
  */
-export default function getMatrixElementsSum(/* matrix */) {
+export default function getMatrixElementsSum(/*matrix*/) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
